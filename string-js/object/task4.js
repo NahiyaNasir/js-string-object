9e2a96ebfbe11ded94d4992485513c7d7685688c
NahiyaNasir <nahiaesha.01@gmail.com>
2024-02-03 00:21:14 +0600

    object added

---
+++ string-js/object/task4.js
@@ -0,0 +1,25 @@
+/*Task-4
+Count the number of properties.
+
+Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+Output:
+
+
+4  
+*/
+ let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+   
+ const count=Object.keys(student).length;
+   console.log(count);
