feb4e5dc65ca6137cd7037eea159869711ceedae
NahiyaNasir <nahiaesha.01@gmail.com>
2024-02-03 00:36:47 +0600

    changes

--- string-js/task3.js
+++ string-js/task3.js
@@ -2,13 +2,15 @@
 Check whether a string contains all the vowels a, e, i, o, u*/
 
 
-let str='Precaution';
- for(i=0; i<str.length; i++){
-      if( str[i].includes('a ')
-    ){
-           
-      }
-          
-      }
+   let hasVowel=' MY NAME IS  SUHAI LOVE BANGLADESH ';
+    hasVowel=hasVowel.toLowerCase();
+    console.log(hasVowel)
+    hasVowel.includes('a')&&
+    hasVowel.includes('i')&&
+    hasVowel.includes('e')&&
+    hasVowel.includes('o')&&
+    hasVowel.includes('u')
+    ? console.log(`All vowels found`)
+    :console.log(`vowels not found`);
      
  
